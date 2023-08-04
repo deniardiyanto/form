2219a2c2a2d7bf6137ebd7b61f0117d013cf1098
Deni Ardiyanto <deniardi4@gmail.com>
2023-08-04 14:29:48 +0700

    update user

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-	var rupiah = document.getElementById('jumlahAwal');
-rupiah.addEventListener('keyup', function(e){
-  // tambahkan 'Rp.' pada saat form di ketik
-  // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
-  rupiah.value = formatRupiah(this.value, '');
-});
-
-/* Fungsi formatRupiah */
-function formatRupiah(angka, prefix){
-  var number_string = angka.replace(/[^,\d]/g, '').toString(),
-  split   		= number_string.split(','),
-  sisa     		= split[0].length % 3,
-  rupiah     		= split[0].substr(0, sisa),
-  ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
-
-  // tambahkan titik jika yang di input sudah menjadi angka ribuan
-  if(ribuan){
-    separator = sisa ? '.' : '';
-    rupiah += separator + ribuan.join('.');
-  }
-
-  rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-  return prefix == undefined ? rupiah : (rupiah ? '' + rupiah : '');
-}
 let jumlahawal = document.getElementById("jumlahAwal");
 let lajupenduduk = document.getElementById("lajuPenduduk");
 var tahunsekarang ;
@@ -38,9 +14,37 @@ document.addEventListener("submit", function(event) {
   // tahunsearch = tahuncari.slice(-4);
   n = tahuncari - tahunsekarang;
   jumlah = parseFloat(jumlahawal.value.replace('.','').replace('.','').replace('.','').replace('.','').replace('.',''));
-  correctAnswer = jumlah*(Math.pow((1+ parseFloat(lajupenduduk.value)),n));
-  result.innerHTML = "Ramalan Jumlah Penduduk tahun "+ tahuncari +" adalah <b>"+correctAnswer;
+  laju = parseFloat(lajupenduduk.value)/100;
+  console.log(laju);
+  correctAnswer = Math.ceil(jumlah*(Math.pow((1+ laju),n)));
+  result.innerHTML = "Ramalan Jumlah Penduduk tahun "+ tahuncari +" adalah <b><div id='jumlahAkhir' value='"+correctAnswer+"'></div>";
   next.style.display = "inline";
+  let rupiah2 =document.getElementById("jumlahAkhir").getAttribute('value');
+// document.addEventListener('keyup', function(e){
+  // tambahkan 'Rp.' pada saat form di ketik
+  // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
+  rupiah2 = formatRupiah2(rupiah2, '');
+  juma = rupiah2;
+  result.innerHTML = "Ramalan Jumlah Penduduk tahun "+ tahuncari +" adalah <b>"+juma;
+
+
+/* Fungsi formatRupiah */
+function formatRupiah2(angka, prefix){
+  let number_string =angka.replace(/[^,\d]/g, '').toString(),
+  split   		= number_string.split(','),
+  sisa     		= split[0].length % 3,
+  rupiah2     		= split[0].substr(0, sisa),
+  ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+
+  // tambahkan titik jika yang di input sudah menjadi angka ribuan
+  if(ribuan){
+    separator = sisa ? '.' : '';
+    rupiah2 += separator + ribuan.join('.');
+  }
+
+  rupiah2 = split[1] != undefined ? rupiah2 + ',' + split[1] : rupiah2;
+  return prefix == undefined ? rupiah2 : (rupiah2 ? '' + rupiah2 : '');
+}
 
 });
 document.addEventListener("click", function(event) {
@@ -54,3 +58,4 @@ document.addEventListener("click", function(event) {
     next.style.display = "none";
   }
 });
+
